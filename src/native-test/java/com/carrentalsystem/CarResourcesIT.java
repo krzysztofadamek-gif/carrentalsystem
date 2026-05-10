fe64692829ec057c0e8bb90b5de7751796a5fb0b
KA <krzysztof.adamek@gmail.com>
2026-05-10 21:38:38 +0200

    Car Rental System Version 0.0.2

--- src/native-test/java/com/carrentalsystem/CarResourcesIT.java
+++
@@ -1,8 +0,0 @@
-package com.carrentalsystem;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class CarResourcesIT extends ReservationResourcesTest {
-    // Execute the same tests but in packaged mode.
-}
